0af0dbec3e6d93e9f72171472993764ddb5988a2
Juan Montoya <juanjmontoya14@gmail.com>
2023-04-08 19:42:39 -0500

    API Gateway deployment

--- get_specific_meet.py
+++ get_specific_meet.py
@@ -1,32 +1,39 @@
 import json
-import boto3
-from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch import Elasticsearch
 
 def lambda_handler(event, context):
-    # Configuración de Elasticsearch
-    host = 'xx' # URL de Elasticsearch
-    username = 'yy' # Nombre de usuario de Elasticsearch
-    password = 'zz' # Contraseña de Elasticsearch
-    
-    # Conexión a Elasticsearch
-    es = Elasticsearch(
-        hosts=[{'host': host}],
-        http_auth=(username, password),
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
-    )
-    
-    # Consulta en Elasticsearch por un documento específico por su ID
-    doc_id = event['doc_id'] # Obtener el ID del documento de la entrada del evento
-    res = es.get(index='meet_records', id=doc_id)
-    
-    # Procesamiento de resultados
-    record = res['_source']
-    
-    # Devolución de respuesta
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(record)
-    }
-    return response
+    try:
+        # Configuración de Elasticsearch
+        host = 'xx' # URL de Elasticsearch
+        username = 'yy' # Nombre de usuario de Elasticsearch
+        password = 'zz' # Contraseña de Elasticsearch
+        
+        # Conexión a Elasticsearch
+        es = Elasticsearch(
+    	    [f"{username}:{password}@{host}"],
+    	    scheme="https",
+    	    request_timeout=30,
+    	    verify_certs=False,
+        )
+        
+        # Consulta en Elasticsearch por un documento específico por su ID
+        body = json.loads(event["body"])
+        doc_id = body['doc_id'] # Obtener el ID del documento de la entrada del evento
+        res = es.get(index='meet_records', id=doc_id)
+        
+        # Procesamiento de resultados
+        record = res['_source']
+        
+        # Devolución de respuesta
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(record)
+        }
+        return response
+    except Exception as e:
+        response = {
+            "statusCode": 500,
+            "body": json.dumps(event)
+        }
+        return response
+
